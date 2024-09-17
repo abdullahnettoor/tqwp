c73dc090771b967900d60f3fd9ddefd385fa0b83
abdullahnettoor <abdullahnettoor@gmail.com>
2024-09-17 13:13:46 +0530

    ⇝ Moved CustomTask to main package - Task file cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/abdullahnettoor/tqwp"
 )
+
+type CustomTask struct {
+	tqwp.TaskModel
+	Id   uint
+	Data any
+}
+
+func (t *CustomTask) Process() error {
+	num, isInt := t.Data.(int)
+	if !isInt {
+		return fmt.Errorf("invalid type")
+	}
+	divisor := rand.Intn(2)
+	if divisor == 0 {
+		return fmt.Errorf("division by zero")
+	}
+	t.Data = num / divisor
+	time.Sleep(time.Millisecond * 10)
+	return nil
+}
 
 func main() {
 
@@ -14,7 +36,7 @@ func main() {
 
 	taskQ := tqwp.NewTaskQueue(numTasks)
 	for i := 1; i <= numTasks; i++ {
-		t := tqwp.CustomTask{
+		t := CustomTask{
 			Id:        uint(i),
 			Data:      rand.Intn(1000),
 			TaskModel: tqwp.TaskModel{},
--- task.go
+++ task.go
@@ -1,12 +1,5 @@
 package tqwp
 
-import (
-	"fmt"
-	"time"
-
-	"math/rand"
-)
-
 // Task is an interface which will be passed
 // through the channel in Taskqueue
 type Task interface {
@@ -35,24 +28,4 @@ func (tm *TaskModel) Retry(maxRetries int) bool {
 
 func (tm *TaskModel) GetRetry() int {
 	return tm.retries
-}
-
-type CustomTask struct {
-	TaskModel
-	Id   uint
-	Data any
-}
-
-func (t *CustomTask) Process() error {
-	num, isInt := t.Data.(int)
-	if !isInt {
-		return fmt.Errorf("invalid type")
-	}
-	divisor := rand.Intn(2)
-	if divisor == 0 {
-		return fmt.Errorf("division by zero")
-	}
-	t.Data = num / divisor
-	time.Sleep(time.Millisecond * 10)
-	return nil
 }
