1fd76a1763c226562bdcd55389276b319a5040ad
abdullahnettoor <abdullahnettoor@gmail.com>
2024-09-20 17:32:48 +0530

    📝 Added doc in task.go

--- task.go
+++ task.go
@@ -1,23 +1,34 @@
 package tqwp
 
-// Task is an interface which will be passed
-// through the channel in Taskqueue
+// Task represents the interface that defines a unit of work.
+// The Process function must be implemented by users to define custom task behavior.
 type Task interface {
-	// Process function will be called in workers to process the task.
-	// This functions must be implemented when creating custom tasks.
+	// Process is the function executed by workers to process the task.
+	// It should return an error if processing fails.
 	Process() error
 }
 
+// RetryableTask is an interface that extends Task and manages retries for failed tasks.
+// It allows tasks to be retried up to a specified maxRetries value.
 type RetryableTask interface {
+	// Task is embedded to ensure users only need to implement the Process method for custom tasks.
 	Task
+
+	// Retry attempts to retry the task, returning true if retries are still allowed.
 	Retry(maxRetries int) bool
+
+	// GetRetry returns the current retry count for the task.
 	GetRetry() int
 }
 
+// TaskModel is a base struct that users can embed in their custom tasks
+// to manage retry logic by keeping track of retry attempts.
 type TaskModel struct {
 	retries int
 }
 
+// Retry increments the retry count and returns true if the task
+// can still be retried (i.e., the retry count is below maxRetries).
 func (tm *TaskModel) Retry(maxRetries int) bool {
 	if tm.retries < maxRetries {
 		tm.retries++
@@ -26,6 +37,7 @@ func (tm *TaskModel) Retry(maxRetries int) bool {
 	return false
 }
 
+// GetRetry returns the number of retries that have been attempted for the task.
 func (tm *TaskModel) GetRetry() int {
 	return tm.retries
 }
